4a9c7600583f51bb9b0333e01ab96569a2d451ca
Marco Giugliani <marco.giugliani@studio.unibo.it>
2023-11-06 15:13:36 +0100

    Started exercise 72

---
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/impl/FunctionImpl.java
@@ -0,0 +1,11 @@
+package it.unibo.functional.impl;
+
+import it.unibo.functional.api.Function;
+
+public class FunctionImpl<I, O> implements Function<I, O> {
+    public O call(I input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'call'");
+    }
+
+}
